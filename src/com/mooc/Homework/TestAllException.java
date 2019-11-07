8897bd1d18141033294b7cb3c0739232a85a5ddb
Haofan Fang <haofanfang@gmail.com>
2019-11-07 10:24:32 +0800

    update MyException

--- src/com/mooc/Homework/TestAllException.java
+++ src/com/mooc/Homework/TestAllException.java
@@ -4,16 +4,25 @@ import java.util.Scanner;
 
 class MyException extends Exception {
 
+    //国际惯例自定义异常类构造
     private static final long serialVersionUID = 1L;
 
-    // 提供无参数的构造方法
     public MyException() {
+        super();
     }
 
-    // 提供一个有参数的构造方法，可自动生成
     public MyException(String message) {
-        super(message);// 把参数传递给Throwable的带String参数的构造方法
+        super(message);
     }
+
+    public MyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MyException(Throwable cause) {
+        super(cause);
+    }
+
 
 }
 
@@ -27,25 +36,27 @@ public class TestAllException {
         }
         //判断什么时候声明异常
         //计算并且返回矩形面积
-
     }
 
     public static void main(String[] args) throws MyException {
         //从键盘上接收矩形的长和宽
         System.out.println("请输入矩形的长和宽，用空格分割：");
         Scanner scanner = new Scanner(System.in);
-        double length = scanner.nextDouble();
-        double width = scanner.nextDouble();
 
-        //调用 方法calculateArea
-        double area = calculateArea(length,width);
 
-        //输出矩形面积
+            //调用 方法calculateArea
         try {
+            double length = scanner.nextDouble();
+            double width = scanner.nextDouble();
+            double area = calculateArea(length, width);
+            //输出矩形面积
             System.out.println("矩形的面积为" + area);
-        } catch (Exception e) {
+        } catch (IllegalArgumentException e) {
+            System.out.println("输入的数字不对"+e.getMessage());
             e.printStackTrace();
         }
+
+
 
         //对于上述操作，注意捕获可能出现的异常
     }
